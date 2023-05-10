9b4b00693f16e78a26fe22121888fa918c92bcab
DaniellaKarlen <daniella.karlen@gmail.com>
2023-05-10 16:17:37 +0200

    navigate

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { landscapeImg } from '../../db/imageDB';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Gallery.css';
 // import landscapeImgHeader from 'https://images.unsplash.com/photo-1511576661531-b34d7da5d0bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
 
 function Gallery() {
-
+  const navigate = useNavigate();
  let img =  landscapeImg[0]
  console.log(img, img.id)
   
@@ -23,7 +24,8 @@ function Gallery() {
           <div className='landscape-card'>
             <div className='landscape-card-info'>
               <h1>Landscape</h1>
-              <button><Link to="/gallery/landscape">See more Landscape</Link></button>
+              {/* <button><Link to="/gallery/landscape">See more Landscape</Link></button> */}
+              <button onClick={() => {navigate('/gallery/landscape')}}>see more landscape</button>
             </div>
            <img src={img.url} alt="headerimg"></img>
           </div>
