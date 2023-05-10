72d1b19b8f277047e79d920cb88a904ceab4af17
DaniellaKarlen <daniella.karlen@gmail.com>
2023-05-10 15:55:43 +0200

    img header

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { landscapeImg, portraitImg, buildingImg } from '../../db/imageDB';
+import { landscapeImg } from '../../db/imageDB';
+import '../styles/Gallery.css';
+// import landscapeImgHeader from 'https://images.unsplash.com/photo-1511576661531-b34d7da5d0bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
 
 function Gallery() {
+
+ let img =  landscapeImg[0]
+ console.log(img, img.id)
   
+
   // let Images = [];
+  
   
 
   return (
@@ -13,9 +20,20 @@ function Gallery() {
 
       <nav>
         <ul>
-          <Link to="/gallery/landscape">Landscape</Link>
-          <Link to="/gallery/portraits">Portraits</Link>
-          <Link to="/gallery/buildings">Buildnings</Link>
+          <div className='landscape-card'>
+            <div className='landscape-card-info'>
+              <h1>Landscape</h1>
+              <button><Link to="/gallery/landscape">See more Landscape</Link></button>
+            </div>
+           <img src={img.url} alt="headerimg"></img>
+          </div>
+          <div className='portrait-card'>
+            <Link to="/gallery/portraits">Portraits</Link>
+          </div>
+          <div className='building-card'>
+            <Link to="/gallery/buildings">Buildnings</Link>
+          </div>
+          
         </ul>
         {/* 
         Outlet är för child-components 
